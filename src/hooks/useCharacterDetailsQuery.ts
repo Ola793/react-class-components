5cc059181f2515bf73ffbc267dcb174d6e869f58
Ola793 <olitieplove@gmail.com>
2026-06-02 12:42:45 +0200

    feat: add query hooks

---
+++ src/hooks/useCharacterDetailsQuery.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchCharacterById } from '../api/charactersApi';
+import { queryKeys } from '../query/queryKeys';
+
+export function useCharacterDetailsQuery(characterId: string | null) {
+  return useQuery({
+    queryKey: queryKeys.characterDetails(characterId ?? ''),
+    queryFn: () => fetchCharacterById(characterId ?? ''),
+    enabled: Boolean(characterId),
+  });
+}
--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,15 @@ import { CharacterDetails } from './components/CharacterDetails.tsx';
 import { AboutPage } from './pages/AboutPage.tsx';
 import { NotFoundPage } from './pages/NotFoundPage.tsx';
 import { ThemeProvider } from './context/ThemeProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<CharacterDetails />} />
@@ -18,6 +22,7 @@ createRoot(document.getElementById('root') as HTMLElement).render(
           <Route path="/about" element={<AboutPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
+        </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
