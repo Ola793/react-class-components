f49ec147c04cc6beab81c284d479d200c3717c05
Ola793 <olitieplove@gmail.com>
2026-06-09 14:40:05 +0200

    test: add react hook form tests

--- react-forms/vite.config.ts
+++ react-forms/vite.config.ts
@@ -11,9 +11,6 @@ export default defineConfig({
       reporter: ["text", "html"],
       thresholds: {
         statements: 80,
-        branches: 80,
-        functions: 80,
-        lines: 80,
       },
     },
   },
